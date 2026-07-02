ad871edb71a041df41d33ec6a4da6100679db7f6
saintsama98 <biradaraditya9712@gmail.com>
2026-07-03 00:07:16 +0530

    added accessor rebalance internal

--- src/lender/assessor.sol
+++ src/lender/assessor.sol
@@ -6,43 +6,40 @@ pragma solidity 0.8.28;
 /// @title Hooke - a thin open tranching protocol
 /// @author adiii.eth
 /// @notice This is minimal proof of concept to define tranching as a whole defi primmitive with credit market layout drawn from
-/// the motivation of a legacy code base from centrifuge tilake github. 
+/// the motivation of a legacy code base from centrifuge tilake github.
 ///  */
 
-import "../math/Math.sol";
-import "../sacred.sol";
+import "../math/math.sol";
+import "./sacred.sol";
 import "../fixed_point.sol";
+import "../auth/auth.sol";
+import "../math/interest.sol";
 
-//inline interfaces 
+//inline interfaces
 
-/// @notice for nav based calculations motivated from borrower side and discounted rate dep  
-interface navLike{
-
-}
+/// @notice for nav based calculations motivated from borrower side and discounted rate dep
+interface navLike {}
 
 /// @notice token interface
-interface trancheLike{
+interface trancheLike {}
 
-}
-/// @notice the idle reserve interface, refer idle,sol 
-interface idleLike{
-
-}
-
-///@notice this depends strategies and future strategy developments, adapter surface may change as per roadmap 
-interface lendingLike{
-
+/// @notice the idle reserve interface, refer idle,sol
+interface idleLike {
+    function totalBalance() external view returns (uint256);
 }
 
-contract assessor is math,sacred,auth{
+///@notice this depends strategies and future strategy developments, adapter surface may change as per roadmap
+interface lendingLike {}
+
+contract assessor is Math, sacred, Auth, Interest {
     Fixed27 public seniorRatio;
 
-    ///@notice accessor is mostly motivated from senior tranche first as priority as per the convention for 
+    ///@notice accessor is mostly motivated from senior tranche first as priority as per the convention for
     /// easy follow up and this convemtion should be maintained for development.
 
     // there are two senior variables that are dedicated to token level calculations
 
-    ///@dev seniorDebt is asset value that brings interest and is playable 
+    ///@dev seniorDebt is asset value that brings interest and is playable
     /// seniorBalance is the balance of the senior tranche that dosen't contribute in interest accruel
     uint256 seniorDebt_;
     uint256 seniorBalance_;
@@ -56,17 +53,17 @@ contract assessor is math,sacred,auth{
 
     /// @dev intentionally lending adapter type engagement is missing, configurable for future integrations
 
-    TrancheLike public seniorTranche;
-    TrancheLike public juniorTranche;
-    NAVFeedLike public navFeed;
-    ReserveLike public reserve;
+    trancheLike public seniorTranche;
+    trancheLike public juniorTranche;
+    navLike public navFeed;
+    idleLike public reserve;
 
     uint256 maxIdle; //reserve
 
     //dust related balance tolerence
-    uint256 public constant supplyTolerence= 5;
+    uint256 public constant supplyTolerence = 5;
 
-    constructor () {
+    constructor() {
         seniorInterestRate.value = ONE;
         lastUpdateSeniorInterest = block.timestamp;
         seniorRatio.value = 0;
@@ -76,26 +73,44 @@ contract assessor is math,sacred,auth{
 
     //======rebalancing========
 
-    function rebalance() public{
-        rebalance(calcExpectedDebtAsset(_accrueSeniorDebt(),_seniorBalance));
+    function rebalance() public {
+        rebalance(calcExpectedSeniorAssets(_accrueSeniorDebt(), _seniorBalance));
     }
 
-    function rebalance(uint256 seniorAsset_) internal{
+    function rebalance(uint256 seniorAsset_) internal {
+        //get nav
+        uint256 nav_ = getNav();
+        //get reserve
+        uint256 reserve_ = reserve.totalBalance();
+
+        //this is the primary implementation of waterfall and thus need other deps like
+        //senior ratio
+
+        uint256 seniorRato_ = calcSeniorRatio(seniorAsset_, nav_, reserve_);
+
+        //debt for senior tranche specefically
+        seniorDebt_ = rmul(nav_, seniorRato_);
+
+        //senior is priority and under loss protection
+        if (seniorDebt_ > seniorAsset_) {
+            seniorDebt_ = seniorAsset_;
+            seniorBalance_ = 0;
+        } else {
+            seniorBalance_ = safeSub(seniorAsset_, seniorDebt_);
+        }
     }
 
     function getNav() public view returns (uint256 _nav) {
-
+        //nav should be
     }
 
     //======helpers========
 
-    function _accrueSeniorDebt() public pure returns (uint256 finalAccruel){
+    function _accrueSeniorDebt() public returns (uint256 finalAccruel) {
         if (lastUpdateSeniorInterest >= block.timestamp) {
-            return chargeInterest();
+            return chargeInterest(seniorDebt_, seniorInterestRate.value, lastUpdateSeniorInterest);
         }
         lastUpdateSeniorInterest = block.timestamp;
         return finalAccruel;
     }
-
-
 }
